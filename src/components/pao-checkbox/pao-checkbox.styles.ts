3f3a319136af72109427538aed7c5c9f9632838d
captainpao <the_synchronizer@hotmail.com>
2026-04-20 23:13:27 +0800

    fix: use proper visually-hidden pattern for pao-checkbox native input

--- src/components/pao-checkbox/pao-checkbox.styles.ts
+++ src/components/pao-checkbox/pao-checkbox.styles.ts
@@ -25,8 +25,13 @@ export const styles = css`
     position: absolute;
     width: 1px;
     height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
     opacity: 0;
-    margin: 0;
   }
 
   .pao-checkbox-control {
